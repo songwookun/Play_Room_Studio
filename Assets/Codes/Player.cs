e3bc46b24c9a2d9fdb0ca7225030d036bb91ef99
songwookun <144450071+songwookun@users.noreply.github.com>
2025-04-24 00:57:13 +0900

    #필드 구현 및 캐릭터 이동 기능 추가(250424)

---
+++ Assets/Codes/Player.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem; // Input System 사용 시 필요
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed = 5f;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void OnMove(InputValue value) 
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    private void LateUpdate() 
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
+}
